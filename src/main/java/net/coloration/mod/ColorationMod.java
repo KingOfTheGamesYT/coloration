16846ebf4f848f5d1b3c83a197bfdbb1693f7be2
KingOfTheGamesYT <53309562+KingOfTheGamesYT@users.noreply.github.com>
2024-04-29 02:38:53 -0400

    added the creative tab
    to 1.16.5 forge

--- src/main/java/net/coloration/mod/ColorationMod.java
+++ src/main/java/net/coloration/mod/ColorationMod.java
@@ -1,5 +1,8 @@
 package net.coloration.mod;
 
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
@@ -9,12 +12,15 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod("colorationmod")
 public class ColorationMod {
+    public static final String MOD_ID = "colorationmod";
+
     public ColorationMod() {
+
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
         final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         modEventBus.addListener(this::setup);
-        //RegistryHandler.init();
+        RegistryHandler.init();
         MinecraftForge.EVENT_BUS.register(this);
     }
 
@@ -22,4 +28,12 @@ public class ColorationMod {
     }
     private void doClientStuff(final FMLClientSetupEvent event) {
     }
+
+    public static final ItemGroup BLOCKS = new ItemGroup("blocks") {
+
+        @Override
+        public ItemStack createIcon() {
+            return new ItemStack(Items.BLUE_DYE);
+        }
+    };
 }
